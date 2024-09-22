e791745eddf86ba440877d833692bd610dd5b38b
KuyaKen <ricewitduhspringroll@gmail.com>
2024-09-21 21:48:51 -0700

    build 1.0.8

--- Scheduler/app/schoology/test.js
+++ Scheduler/app/schoology/test.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { getRandomValues } from 'react-native-get-random-values';
 import * as WebBrowser from 'expo-web-browser';
-import * as Linking from 'expo-linking';
-import * as AuthSession from 'expo-auth-session';
+import * as Crypto from 'expo-crypto';
 
 
 const Key = 'efe5a543948026772946115c316323cd05e443b00';
@@ -13,12 +12,12 @@ async function getRequestToken(Key, Secret) {
   const array = new Uint8Array(8); 
   crypto.getRandomValues(array); 
   const oauthNonce = Array.from(array).map(byte => byte.toString(16).padStart(2, '0')).join(''); 
-  
-
+   
   const URL = `https://api.schoology.com/v1/oauth/request_token?oauth_consumer_key=${Key}&oauth_timestamp=${oauthTimestamp}&oauth_signature_method=PLAINTEXT&oauth_version=1.0&oauth_nonce=${oauthNonce}&oauth_signature=${encodeURIComponent(Secret + '%26')}`;
 
   try {
     const response = await axios.get(URL);
+    console.log("request_token: " + response.data);
     const params = new URLSearchParams(response.data);
         return {
             oauth_token: params.get('oauth_token'),
@@ -33,18 +32,24 @@ export async function promptAuthorization() {
   try {
     const requestToken = await getRequestToken(Key, Secret);
     console.log("oauth_token: " + requestToken.oauth_token);
-    console.log("oauth_token: " + requestToken.oauth_token_secret);
+    console.log("oauth_token_secret: " + requestToken.oauth_token_secret);
 
     if (requestToken) {
-      const redirectURI = AuthSession.makeRedirectUri({
-        useProxy: true,
-      });
-      console.log(`redirectURI: ${redirectURI}`);
       const url = `https://lms.lausd.net/oauth/authorize?oauth_consumer_key=${Key}&oauth_token=${requestToken.oauth_token}&oauth_token_secret=${requestToken.oauth_token_secret}`;
       console.log(url);
-      const result = await WebBrowser.openAuthSessionAsync(url, redirectURI);
-      console.log('InAppBrowser result:', result);
-    }
+      const result = await WebBrowser.openAuthSessionAsync(url);
+      if (result.type == "cancel") {
+        const array = new Uint8Array(8); 
+        crypto.getRandomValues(array); 
+        const oauthNonce = Array.from(array).map(byte => byte.toString(16).padStart(2, '0')).join(''); 
+        const oauthTimestamp = Math.floor(Date.now() / 1000);
+
+        const studentUrl = `https://api.schoology.com/v1/courses/7354453829/assignments?oauth_consumer_key=${Key}&oauth_token=${requestToken.oauth_token}&oauth_signature_method=PLAINTEXT&oauth_version=1.0&oauth_nonce=${oauthNonce}&oauth_timestamp=${oauthTimestamp}&oauth_signature=${Secret}&${requestToken.oauth_token_secret}`;
+        console.log(studentUrl);
+        const studentResult = await axios.get(studentUrl);
+        console.log(studentResult.data);
+        }
+      }
   } catch (error) {
     console.error('Error during authorization:', error);
   }
