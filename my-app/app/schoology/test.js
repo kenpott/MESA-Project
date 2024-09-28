cfcc77f4c6a6a69aa67c02f79e3fa01751cbe07c
KuyaKen <ricewitduhspringroll@gmail.com>
2024-09-28 12:03:03 -0700

    Update test.js

--- my-app/app/schoology/test.js
+++ my-app/app/schoology/test.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getRandomValues } from 'react-native-get-random-values';
-import * as WebBrowser from 'expo-web-browser';
+import { openAuthSessionAsync } from 'expo-web-browser';
 import * as Crypto from 'expo-crypto';
 
 const Key = 'efe5a543948026772946115c316323cd05e443b00';
@@ -23,7 +23,7 @@ export async function promptAuthorization() {
   const url = `${domain}oauth/authorize?oauth_consumer_key=${Key}&oauth_token=${Token}&oauth_token_secret=${TokenSecret}`;
   console.log(`Authorization Url: ${url}`);
   
-  const result = await WebBrowser.openAuthSessionAsync(url);
+  const result = await openAuthSessionAsync(url);
   if (result.type === "cancel") {
     const studentUrl = `${baseUrl}sections/7354453829/assignments?oauth_consumer_key=${Key}&oauth_nonce=${oauthNonce}&oauth_signature=${Secret}%26&oauth_signature_method=PLAINTEXT&oauth_timestamp=${Math.floor(Date.now() / 1000)}&oauth_version=1.0`;
     console.log("Student Url: " + studentUrl);
