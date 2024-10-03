bcf9594418eed4c9767194eb6cbb4f6d4bc20475
KuyaKen <ricewitduhspringroll@gmail.com>
2024-10-03 15:21:13 -0700

    .

---
+++ my-app/app/(tabs)/dashboard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Tab() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-neutral-950">
+      <Text className="text-lg text-white">
+        Dashboard
+      </Text>
+    </SafeAreaView>
+  );
+}
