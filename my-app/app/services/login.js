5f8471c45b032221bb05838f3e3e40c95dfd3116
KuyaKen <ricewitduhspringroll@gmail.com>
2024-10-15 18:01:32 -0700

    .

--- my-app/app/services/login.js
+++ my-app/app/services/login.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-async function login() {
-    const { name, value } = await axios.post('http://localhost:8080/login', {
-        email: 'ksalas0022@mymail.lausd.net',
-        password: 'Thisismynewpassword',
-    }).data;
+async function login(email, password) {
+    const result = await axios.post('http://192.168.1.95:8000/login', {
+        email: email,
+        password: password,
+    });
+    const {name, value} = result.data
 
     console.log(name, value);
     return {name, value};
