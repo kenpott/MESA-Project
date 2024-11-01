f47b2139a1b47f52286f232b6b6baecbd9363ad7
KuyaKen <ricewitduhspringroll@gmail.com>
2024-10-31 19:59:46 -0700

    Update fetchassignments.ts

--- scheduler/app/services/fetchassignments.ts
+++ scheduler/app/services/fetchassignments.ts
@@ -22,59 +22,9 @@ interface AssignmentsData {
 export async function fetchAllAssignments(): Promise<AssignmentsData> {
   const cookie = await getCookie();
 
-  const [upcomingResponse, overdueResponse] = await Promise.all([
-    axios.get(`${domain}/home/upcoming_submissions_ajax`, { headers: { "Cookie": cookie } }),
-    axios.get(`${domain}/home/overdue_submissions_ajax`, { headers: { "Cookie": cookie } }),
-  ]);
+  const response = await axios.post('http://192.168.1.95:8000/getAssignments', { "cookie": cookie });
 
-  const upcomingIds = await getAssignmentIds(upcomingResponse.data.html);
-  const overdueIds = await getAssignmentIds(overdueResponse.data.html);
-
-  const upcomingDetails = await fetchDetails(upcomingIds);
-  const overdueDetails = await fetchDetails(overdueIds);
-
-  const assignmentsData: AssignmentsData = {
-    upcoming: {},
-    overdue: {},
-  };
-
-  upcomingIds.forEach((id, index) => {
-    assignmentsData.upcoming[id] = upcomingDetails[index];
-  });
-
-  overdueIds.forEach((id, index) => {
-    assignmentsData.overdue[id] = overdueDetails[index];
-  });
-
-  return assignmentsData;
-}
-
-async function fetchDetails(ids: string[]): Promise<Assignment[]> {
-  const details: Assignment[] = [];
-  for (const id of ids) {
-    const detail = await fetchAssignmentDetails(id);
-    details.push(detail);
-  }
-  return details;
-}
-
-async function getAssignmentIds(html: string): Promise<string[]> {
-  const response = await axios.post('http://192.168.1.95:8000/getAssignmentIds', { html });
   return response.data;
-}
-
-async function fetchAssignmentDetails(id: string): Promise<Assignment> {
-  const cookie = await getCookie();
-
-  const assignmentDetailsResponse = await axios.get(`${domain}/assignment/${id}`, {
-    headers: { "Cookie": cookie },
-  });
-
-  const parsedAssignmentDetails = await axios.post('http://192.168.1.95:8000/parseAssignment', {
-    html: assignmentDetailsResponse.data,
-  });
-
-  return parsedAssignmentDetails.data;
 }
 
 async function getCookie(): Promise<string> {
